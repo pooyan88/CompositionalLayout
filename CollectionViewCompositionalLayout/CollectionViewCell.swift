7f3c31dd7f02630ed4f39d0c20dfc95b5d5099ef
Pooyan J <pooyanjavidi@gmail.com>
2025-03-20 08:56:46 +0330

    first commit

---
+++ CollectionViewCompositionalLayout/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewCompositionalLayout
+//
+//  Created by Pooyan J on 12/29/1403 AP.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
